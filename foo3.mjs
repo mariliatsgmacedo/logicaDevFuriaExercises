fefe6184c8f81eceb904ba18054fc68050df4a9d
Marilia Macedo <maritamisguemacedo@gmail.com>
2020-10-16 15:16:47 +0100

    Starter next level

---
+++ foo3.mjs
@@ -0,0 +1,27 @@
+import assert from 'assert';
+
+/**Lista de Exercícios nível 4 */
+
+
+/** Exercicio Um  */
+
+/** Exercicio Dois  */
+
+/** Exercicio TrÊs  */
+
+/** Exercicio Quatro  */
+
+/** Exercicio Cinco  */
+
+/** Exercicio Seis  */
+
+/**Exercicio Sete  */
+
+/**Exercicio Oito  */
+
+/**Exercicio Nove  */
+
+/**Exercicio 10  */
+
+
+
